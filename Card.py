8b5fec77a62d22eaa386200cf6086e997bbd5c08
Christian Brandalise <christian.brandalise@gmail.com>
2016-01-28 20:44:05 -0600

    adds Card class

---
+++ Card.py
@@ -0,0 +1,7 @@
+class Card(object):
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+    def print_card(self):
+        print self.suit, self.rank
--- Deck.py
+++ Deck.py
@@ -1,6 +1,19 @@
+from Card import Card
+
+
 class Deck(object):
 
     card_suit = ["Clubs", "Diamonds", "Hearts", "Spades"]
-    card_rank = ["Ace", "1", "2", "3", "4", ]
+    card_rank = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
     def __init__(self):
         self.deck = []
+
+        for suit in Deck.card_suit:
+            for rank in Deck.card_rank:
+                c = Card(suit, rank)
+                self.deck.append(c)
+
+        print self.deck
+
+
+d = Deck()
